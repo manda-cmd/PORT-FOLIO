2fb2969628c31fbfc217f0050d571ff058af37bd
manda-cmd <mimifagna@gmail.com>
2025-11-30 22:49:02 +0000

    finition

--- js/pratique.js
+++ js/pratique.js
@@ -1,4 +1,14 @@
 let btn = document.getElementById("btn-1");
+let cv = document.getElementById("cv")
+let temoin = document.getElementById("temoin")
 btn.addEventListener("click", function(){
-    window.location.href="page7.html";
+    window.location.href="html/page7.html";
 });
+cv.addEventListener("click", function(){
+    window.location.href="ABOUKEY_Manda-Esso_CV/index.html";
+});
+temoin.addEventListener("click", function(){
+    window.location.href="html/page6.html";
+});
+
+
